a61148839ab3e2ba27dc51e99733200bed43f283
Ana Paola Juárez Jalomo <anajuarez7@outlook.com>
2024-03-06 17:42:07 -0600

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,68 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require './PHPMailer/Exception.php';
+require './PHPMailer/PHPMailer.php';
+require './PHPMailer/SMTP.php';
+
+function limpiarDatos($datos)
+{
+    $datos = trim($datos);
+    $datos = stripslashes($datos);
+    $datos = htmlspecialchars($datos);
+    return $datos;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = limpiarDatos($_POST['introducir_nombre']);
+    $email = limpiarDatos($_POST['introducir_email']);
+    $telefono = limpiarDatos($_POST['introducir_telefono']);
+    $asunto = limpiarDatos($_POST['introducir_asunto']);
+    $mensaje = limpiarDatos($_POST['introducir_mensaje']);
+    $destinatario = 'anajuarez7@outlook.com';
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: error.php?msg=Correo electrónico no válido");
+        exit();
+    }
+
+    if (empty($nombre) || empty($email) || empty($asunto) || empty($mensaje)) {
+        header("Location: error.php?msg=Todos los campos son obligatorios");
+        exit();
+    }
+
+    $mensajeCompleto = "Nombre: $nombre\nEmail: $email\nTeléfono: $telefono\nAsunto: $asunto\nMensaje: $mensaje\n";
+
+    $mail = new PHPMailer(TRUE);
+    try {
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.example.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'anajrzj@gmail.com';
+        $mail->Password   = 'Mooncherry08#';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 587;
+    
+        $mail->setFrom($email, $nombre);
+        $mail->addAddress($destinatario);
+    
+        $mail->isHTML(true);
+        $mail->Subject = 'Nuevo mensaje de contacto';
+        $mail->Body = $mensajeCompleto;
+    
+        if ($mail->send()) {
+            header("Location: ruesga.php?msg=Correo enviado con éxito");
+            exit();
+        } else {
+            header("Location: error.php?msg=Error al enviar el mensaje: " . $mail->ErrorInfo);
+            exit();
+        }
+    } catch (Exception $e) {
+        header("Location: error.php?msg=Error inesperado: " . $e->getMessage());
+        exit();
+    }
+}
+?>
